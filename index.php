9704900c463edc37e9d248a5238fb68464e936ea
mriyani <ryaneq@gmail.com>
2026-02-09 15:07:17 +0800

    Refactor routing using router.php

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
 <?php
 
 require 'functions.php';
-
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-// dd($uri);
-
-$routes = [
-    '/'        => 'controllers/index.php',
-    '/about'   => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php',
-];
-
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-} else {
-    http_response_code(404);
-    require 'controllers/404.php'; // optional
-}
+require 'router.php';
