987433bcfb9f61029fbd21828bec2946b362cec3
mriyani <ryaneq@gmail.com>
2026-02-21 10:11:36 +0800

    security: implement form validations

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -7,12 +7,28 @@ $db = new Database($config['database']);
 $heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
 
-    header('Location: /notes');
+    $errors = [];
+
+    // Form empty validation
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'Note body is required.';
+    }
+
+    // Form max-characters validation
+    if (strlen($_POST['body']) > 255) {
+        $errors['body'] = 'Note body cannot be more than 255 characters.';
+    }
+
+    if (empty($errors)) {
+
+        $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+
+        header('Location: /notes');
+    }
 }
 
 require 'views/note-create.view.php';
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -8,7 +8,10 @@
         <form method="POST">
             <div class="mb-4">
                 <label for="body" class="block text-gray-700 font-bold mb-2">New Note</label>
-                <input type="text" id="body" name="body" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Here's an idea for a note...">
+                <input type="text" id="body" name="body" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Here's an idea for a note..." value="<?= htmlspecialchars($_POST['body'] ?? '') ?>">
+                <?php if (isset($errors['body'])): ?>
+                    <p class="text-red-500 text-s italic mt-2"><?= $errors['body'] ?></p>
+                <?php endif; ?>
             </div>
             <div class="flex items-center justify-between mt-4">
                 <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline">Create</button>
