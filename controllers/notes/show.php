61191211e670de9a0dbd0d25d6678da8b5f550b7
mriyani <ryaneq@gmail.com>
2026-02-23 16:57:39 +0800

    Handle multiple request methods from a controller

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -9,11 +9,23 @@ $db = new Database($config['database']);
 
 $currentUserId = 1;
 
-$note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->findOrFail();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->findOrFail();
 
-authorize($note['user_id'] === $currentUserId);
+    authorize($note['user_id'] === $currentUserId);
 
-view('notes/show.view.php', [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    // Form was submitted, delete the note
+    $db->query('DELETE FROM notes WHERE id = :id', ['id' => $_POST['id']]);
+    header('Location: /notes');
+    exit();
+} else {
+    // Show the note
+    $note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    view('notes/show.view.php', [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -12,6 +12,10 @@
         <p class="mt-2">
             <?= htmlspecialchars($note['body']) ?>
         </p>
+        <form class="mt-6" method="POST">
+            <input type="hidden" name="id" id="id" value="<?= $note['id'] ?>">
+            <button class="text-red-500">Delete Note</button>
+        </form>
     </div>
 </main>
 
